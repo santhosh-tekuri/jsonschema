a3ca3edb1be2a6b59a754900c2f9f0247f46fdb9
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2021-08-28 19:31:07 +0530

    Remove BSD license header

--- httploader/httploader.go
+++ httploader/httploader.go
@@ -1,7 +1,3 @@
-// Copyright 2017 Santhosh Kumar Tekuri. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 // Package httploader implements loader.Loader for http/https url.
 //
 // The package is typically only imported for the side effect of
