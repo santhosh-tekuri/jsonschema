396eddf1a43f009161c24f5b96c49d7cb70f2884
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2021-09-11 20:40:39 +0530

    tests: update to JSON-Schema-Test-Suite@72e31dd

--- schema_test.go
+++ schema_test.go
@@ -21,7 +21,7 @@ import (
 	_ "github.com/santhosh-tekuri/jsonschema/v5/httploader"
 )
 
-var testSuite = "testdata/JSON-Schema-Test-Suite@66e813a"
+var testSuite = "testdata/JSON-Schema-Test-Suite@72e31dd"
 
 var skipTests = map[string]map[string][]string{
 	"TestDraft4/optional/unicode.json": {}, // golang regex works on ascii only
